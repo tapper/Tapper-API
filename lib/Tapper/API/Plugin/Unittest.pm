518e3150eb3c38ec1b33dc969f5651ebdc205f49
Maik Hentsche <caldrin@amazon.com>
2013-09-17 13:58:24 +0200

    [FEAT] Tapper Web API
    This is the basic framework for the Tapper Web API. It only routes to
    the modules that handle the URLs recognized by the API and that do the
    actual work.

---
+++ lib/Tapper/API/Plugin/Unittest.pm
@@ -0,0 +1,40 @@
+package Tapper::API::Plugin::Unittest;
+
+use warnings;
+use strict;
+use 5.010;
+
+use Mojolicious::Lite;
+use JSON::XS;
+
+put 'unit-test/:testrun' => sub {
+        my $self = shift;
+        $self->app->renderer->default_format('json');
+
+
+        my $data;
+        if ($self->tx->req->body) {
+                $data = eval{JSON::XS::decode_json($self->tx->req->body)};
+                if ($@) {
+                        $self->render(json => {
+                                               error => $@
+                                              },
+                                      status => 409,
+                                     );
+                        return;
+                }
+        }
+        if (not $data->{foo} eq 'bar') {
+                $self->render(json => {
+                                       error => "Can not find key 'foo' with value 'bar' in JSON body",
+                                      },
+                              status => 409,
+                             );
+        }
+        $self->respond_to(json => {json => {key => 'value'},
+                                   status => 202,
+                                  },
+                         );
+};
+
+1;
